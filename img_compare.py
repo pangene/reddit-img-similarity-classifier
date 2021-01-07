9de71bf7492f29ab746ece448842ce27bf0d83c5
Gene Pan <pangene@gmail.com>
2021-01-07 03:06:39 -0800

    Created classifcation function. Made getting images function. Changed show_images function to be my own and faster. Basically done.

--- img_compare.py
+++ img_compare.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import imagehash
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
@@ -7,52 +8,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+# Logging setup
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(name)s:%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG)
+
+logging.getLogger('skimage').setLevel(logging.WARNING)
+logging.getLogger('PIL').setLevel(logging.WARNING)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+
 TEST_IMG_PATH = 'test_images/'
-MSE_CUTOFF = 0.1  # Lower MSE is more similar 
+MSE_CUTOFF = 0.08  # Lower MSE is more similar
 SSIM_CUTOFF = 0.6  # Higher SSIM is more similar
-HASH_CUTOFF = 30. # Lower hash difference is more similar
-
-def mse(imgA, imgB):
-    '''Returns the Mean Squared Error (MSE) between the two images.'''
-    assert imgA.shape == imgB.shape
-    err = np.sum((imgA.astype("float") - imgB.astype("float")) ** 2)
-    err /= float(imgA.shape[0] * imgA.shape[1])
-    return err
+HASH_CUTOFF = 30 # Lower hash difference is more similar
 
 
-# Taken from https://gist.github.com/soply/f3eec2e79c165e39c9d540e916142ae1
-# with some modifications
-def show_images(images, cols=1, titles=None):
-    """Display a list of images in a single figure with matplotlib.
+# UTILITY FUNCTIONS
 
-    Parameters
-    ---------
-    images: List of np.arrays compatible with plt.imshow.
+test_img_path = lambda img_name: os.path.join(TEST_IMG_PATH, img_name)
 
-    cols (Default = 1): Number of columns in figure (number of rows is
-                        set to np.ceil(n_images/float(cols))).
 
-    titles: List of titles corresponding to each image. Must have
-            the same length as titles.
-    """
-    assert((titles is None) or (len(images) == len(titles)))
-    n_images = len(images)
-    if titles is None:
-        titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
-    fig = plt.figure()
-    for n, (image, title) in enumerate(zip(images, titles)):
-        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
-        if image.ndim == 2:
-            plt.gray()
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        a.set_title(title)
-    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+def show_images(images, titles=None, cols=4, show=None):
+    '''Display a list of images in a single figure with matplotlib.'''
+    if titles:
+        assert len(images) == len(titles)
+    if show:
+        images = images[:show]
+    rows = len(images) // cols + 1
+    for i, img in enumerate(images):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.subplot(rows, cols, i + 1)
+        if titles:
+            plt.title(titles[i])
+        else:
+            plt.title(f'Image {i + 1}')
+        plt.axis('off')
+        plt.imshow(img)
+    plt.tight_layout()
     plt.show()
 
 
 def img_folder_plot(path_img_folder):
     '''Plots all the images in a folder and returns a list of image paths.'''
     image_names = os.listdir(path_img_folder)
+    image_names = filter(lambda x: not x.startswith('.'), image_names)
+    image_names = list(image_names)
     image_names.sort()
     images = []
     image_paths = []
@@ -60,8 +62,18 @@ def img_folder_plot(path_img_folder):
         image_path = os.path.join(path_img_folder, image_name)
         image_paths.append(image_path)
         images.append(cv2.imread(image_path))
-    show_images(images, titles=image_paths)
+    show_images(images, titles=image_names)
     return image_paths
+
+# COMPARISON STUFF
+# TODO: Should this be a class?
+
+def mse(imgA, imgB):
+    '''Returns the Mean Squared Error (MSE) between the two images.'''
+    assert imgA.shape == imgB.shape
+    err = np.sum((imgA.astype("float") - imgB.astype("float")) ** 2)
+    err /= float(imgA.shape[0] * imgA.shape[1])
+    return err
 
 
 def compare_images(imgA, imgB, visualize=False):
@@ -87,20 +99,87 @@ def compare_images(imgA, imgB, visualize=False):
     return similarity_score
 
 
-def similarity_func(imgA, imgB, \
+def compare_all_images(img_list, visualize=False):
+    '''Compares all images in the list.'''
+    while img_list:
+        imgA = img_list[0]
+        for img in img_list[1:]:
+            imgB = img
+            compare_images(imgA, imgB, visualize=visualize)
+        img_list = img_list[1:]
+
+
+def det_similar(imgA=None, imgB=None, similarity_score=None, \
         MSE_cutoff=MSE_CUTOFF, SSIM_cutoff=SSIM_CUTOFF, HASH_cutoff=HASH_CUTOFF):
     '''Returns True or False if two images are deemed similar.'''
-    similarity_score = compare_images(imgA, imgB)
-    if similarity_score[0] < MSE_CUTOFF or similarity_score[1] > SSIM_CUTOFF or\
-        similarity_score[2] < HASH_CUTOFF:
+    assert imgA and imgB and not similarity_score \
+            or similarity_score and not imgA and not imgB
+    if not similarity_score and imgA and imgB:
+        similarity_score = compare_images(imgA, imgB)
+    if similarity_score[0] < MSE_cutoff and similarity_score[1] > SSIM_cutoff and\
+        similarity_score[2] < HASH_cutoff:
         return True
     return False
 
 
-test_img_path = lambda img_name: os.path.join(TEST_IMG_PATH, img_name)
+def classifier_func(img_list, visualize=False, verbose=False):
+    '''
+    Returns a list of lists, with each nested list being a category of similar
+    images.
+    '''
+    logging.info('Classifying...')
+    if not verbose:
+        orig_img_list = img_list[:]
+    categories = []
+    i = 1  # Used for debugging + to help track sim scores
+    while img_list:
+        logging.info('Grouping new category...')
+        imgA = img_list[0]
+        category = [imgA]
+        other_images = img_list[1:]
+        for img in other_images:  # TODO: Some way to track which images were deemd similar from original list.
+            imgB = img
+            sim_score = compare_images(imgA, imgB)
+            if det_similar(similarity_score=sim_score):
+                if verbose:
+                    logging.info(f'Images were deemed similar')
+                else:
+                    other_index = None
+                    for j, img in enumerate(orig_img_list[i - 1:]):
+                        if np.array_equal(img, imgB):
+                            other_index = j + i
+                            break
+                    assert other_index > i
+                    logging.info(f'Images {i} and {other_index} were deemed similar')
+                logging.debug(f'Similarity_score: {sim_score}')
+                category.append(imgB)
+                # Note: list.remove fails with a list of np arrays
+                index = 1
+                for img in img_list[1:]:  # This can be optimized.
+                    if np.array_equal(img, imgB):
+                        img_list.pop(index)
+                        break
+                    index += 1
+        categories.append(category)
+        i += 1
+        img_list = img_list[1:]
+    logging.info('Done classifying')
+    if visualize:
+        for i, category in enumerate(categories):
+            print(f'Category {i + 1}:')
+            show_images(category)
+    return categories
+
 
 def main():
-    pass
+    test_image_names = os.listdir(TEST_IMG_PATH)
+    test_image_names = filter(lambda x: not x.startswith('.'), test_image_names)
+    test_image_names = list(test_image_names)
+    test_image_names.sort()
+    test_image_paths = [test_img_path(image_name) for image_name in test_image_names]
+    test_images = [cv2.imread(image_path) for image_path in test_image_paths]
+    # compare_all_images(test_images, visualize=True)
+    classifier_func(test_images, visualize=True)
 
 
 if __name__ == '__main__':
--- reddit_img_classifier.py
+++ reddit_img_classifier.py
@@ -1,8 +1,57 @@
-from img_compare import *
+import logging
+import requests
+import img_compare
 import praw
+from skimage import io
+import cv2
 
-reddit=praw.Reddit('reddit_img_classifier',
+# Logging setup
+logging.basicConfig(
+    format='''%(asctime)s,%(msecs)d %(levelname)-8s [%(name)s:%(filename)s:%(lineno)d] %(message)s''',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG)
+
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+logging.getLogger('prawcore').setLevel(logging.WARNING)
+logging.getLogger('skimage').setLevel(logging.WARNING)
+logging.getLogger('PIL').setLevel(logging.WARNING)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+reddit = praw.Reddit('reddit_img_classifier',
     user_agent='meme_classifier: by u/Puzzled_Yellow')
 
-print(reddit.read_only)
-print(reddit.user.me())
+# Works best on subreddits dedicated to memes or just image posts.
+def get_top_images(subreddit='all', time='month', post_num=20):
+    '''
+    Returns a list of cv2 images(numpy arrays) from the specified subreddit's
+    top x posts (where x=post_num) for the specified time period.
+    '''
+    logging.info('Acquiring %s images from %s for the last %s',
+        post_num, subreddit, time)
+    subreddit = reddit.subreddit(subreddit)
+    iter_submission = subreddit.top(time)
+    images = []
+    while len(images) < post_num:
+        try:
+            submission = next(iter_submission)
+        except StopIteration:
+            logging.info('No more submissions to search')
+            logging.info('Images acquired: %s', len(images))
+            break
+        url = submission.url
+        if url.endswith('jpg') or url.endswith('jpeg') or url.endswith('png'):
+            logging.debug('Acquiring image %s from %s', len(images) + 1, url)
+            image = io.imread(url)  # Reads as RGB
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # All images in img_compare assume BGR
+            images.append(image)
+    logging.info('Done acquiring images')
+    return images
+
+def main():
+    images = get_top_images('ProgrammerHumor')
+    # img_compare.show_images(images)
+    img_compare.classifier_func(images, visualize=True)
+
+if __name__ == '__main__':
+    main()
